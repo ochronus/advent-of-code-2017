73c5ad0552af57c2de4f68f9bc3a2b1b8d41f22a
Csaba Okrona <ochronus@ochronus.com>
2017-12-06 05:27:25 +0100

    Day 5 in Racket

--- day5.rkt
+++ day5.rkt
@@ -1,3 +1,26 @@
 #lang racket
 
-(display "hello world\n")
+(define instructions (map string->number (file->lines "day5-input.txt")))
+
+
+(define (walk1 instructions pos acc)
+  (if (or (>= pos  (length instructions)) (< pos 0))
+      (println acc)
+      (let ([jump (list-ref instructions pos)])
+        (walk1 (list-set instructions pos (+ jump 1)) (+ pos jump) (+ acc 1)))))
+
+(define (walk2 instructions pos acc)
+  (if (or (>= pos  (length instructions)) (< pos 0))
+      (println acc)
+      (let ([jump (list-ref instructions pos)])
+        (walk2 (list-set instructions pos
+                         (if (>= jump 3)
+                             (- jump 1)
+                             (+ jump 1))
+                         )
+                        
+               (+ pos jump)
+               (+ acc 1)))))
+
+(walk1 instructions 0 0)
+(walk2 instructions 0 0)
