a613dc836740b9226b460518827c099552d0584c
Csaba Okrona <ochronus@ochronus.com>
2017-12-08 06:27:35 +0100

    Day 8 simplified

--- day8.py
+++ day8.py
@@ -4,23 +4,17 @@ from collections import defaultdict
 code_lines = open("day8-input.txt").read().strip().splitlines()
 
 registers = defaultdict(int)
-
-def compute(reg_to_check, comp_op, val_to_compare_with):
-    left = str(registers[reg_to_check])
-    # register value [comparison operator] compared_value
-    return eval(left + comp_op + val_to_compare_with)
-
 max_value_in_any_reg = 0
-for code_line in code_lines:
 
+for code_line in code_lines:
     reg_to_mod, op, value, _, reg_to_check, comp_op, val_to_compare_with = code_line.strip().split()
 
-    if compute(reg_to_check, comp_op, val_to_compare_with):
+    if eval("registers[reg_to_check]" + comp_op + val_to_compare_with):
         if op == 'inc':
             registers[reg_to_mod] += int(value)
         else:
             registers[reg_to_mod] -= int(value)
-            
+
         max_value_in_any_reg = max(registers[reg_to_mod], max_value_in_any_reg)
 
 
